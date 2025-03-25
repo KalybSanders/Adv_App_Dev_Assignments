4fe358e0b4725a08f0009fba564436189d3b3a9e
KalybSanders <60753245+KalybSanders@users.noreply.github.com>
2025-03-24 21:58:41 -0400

    Added Assignment4 and py programs

---
+++ Assignment4/ProgramA.py
@@ -0,0 +1,37 @@
+# Program Name: ProgramA.py
+# Course: IT3883/Section 17540
+# Student Name: Kalyb Sanders
+# Assignment Number: 4
+# Due Date: 03/ 24/ 2025
+# Purpose: This program establishes a socket connection between two computers. The first computer (Program A)
+# inputs a string to send to the second computer (ProgramB), when B receives this message, it
+# sets the entire string to upper case and sends it back to A.
+#
+# List Specific resources used to complete the assignment.
+#   I used the reading provided in the class
+
+import socket
+
+# create a socket. Bind and listen on 127.0.0.1 (localhost) on port 40210
+s = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+s.bind(("127.0.0.1", 40210))
+s.listen()
+conn, addr = s.accept()  # Blocks
+
+# send a message to program B.
+msg = input("Input a string to send: ").encode('utf-8')
+conn.sendall(msg)
+
+# receive message from B (the entered message in upper case).
+new_msg = conn.recv(256).decode('utf-8')
+# altered_message = altered_text.decode('utf-8')
+
+# print the altered message
+print(new_msg)
+
+# close the socket.
+conn.sendall(b'Closing connection...')
+s.close()
---
+++ Assignment4/ProgramB.py
@@ -0,0 +1,35 @@
+# Program Name: ProgramB.py
+# Course: IT3883/Section 17540
+# Student Name: Kalyb Sanders
+# Assignment Number: 4
+# Due Date: 03/ 24/ 2025
+# Purpose: This program establishes a connection between two computers. The first computer (Program A)
+# inputs a string to send to the second computer (ProgramB), when B receives this message, it
+# sets the entire string to upper case and sends it back to A.
+#
+# List Specific resources used to complete the assignment.
+#   I used the reading provided in the class
+
+import socket
+
+# create a socket and establish connection to 127.0.0.1 (localhost) on port 40210
+s = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+s.connect(("127.0.0.1", 40210))
+
+# receive message from A.
+received_msg = s.recv(256).decode('utf-8')
+
+# set the message to uppercase
+uppercase_msg = received_msg.upper().encode('utf-8')
+
+# send the message back on the socket to A.
+s.sendall(uppercase_msg)
+
+# print the socket close message
+print(s.recv(256).decode('utf-8'))
+
+# close the program
+s.close()
